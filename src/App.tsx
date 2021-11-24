c704349ca1dfd5a857d6f9e0f7c6f1df6dea4855
Noro190695 <norojan56mail.ru>
2021-11-24 09:56:52 +0400

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
+import { url } from 'inspector';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import bg from './images/background/bookwood_linuxmint.jpg';
+console.log(bg);
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="app" style={{backgroundImage: url('bg')}}>
+    <div>
+       
     </div>
   );
 }
---
+++ src/components/panel/Panel.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import panelStyle from './panel.module.scss';
+
+
+const Panel: React.FC = ({ children }) => {
+    return (
+        <div className={panelStyle.panel}>
+
+        </div>
+    )
+}
